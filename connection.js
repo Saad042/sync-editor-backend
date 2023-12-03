60d7708a55a5fa5efb2f5e348429e50ecba0e90b
Saad Aftab <69530708+Saad042@users.noreply.github.com>
2023-12-03 20:30:21 +0500

    debug mongodb connection

--- connection.js
+++ connection.js
@@ -4,6 +4,7 @@ mongoose.set('strictQuery', false);
 exports.createConnection = async function (mongoDbUri) {
   try {
     await mongoose.connect(mongoDbUri);
+    console.log('Connection to mongodb created succesfully');
   } catch (error) {
     console.log('Connection to mongodb failed', error);
   }
